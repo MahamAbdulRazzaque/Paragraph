9168c7577a05f9a9831eb8c61dd68e76600bc824
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-23 17:10:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// function paragraph() {
+//     let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptate, accusantium magnam, excepturi necessitatibus voluptatibus officia doloribus facere repellat doloremque totam rerum voluptatem dolorum obcaecati expedita, asperiores ducimus nisi similique nam tenetur tempore? Dignissimos Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta voluptate, accusantium magnam, excepturi necessitatibus voluptatibus officia doloribus facere repellat doloremque totam rerum voluptatem dolorum obcaecati expedita, asperiores ducimus nisi similique nam tenetur tempore? Dignissimos";
+//     document.getElementById("readmore").textContent = text;   
+// }
+
+// function listitem() {
+//     let content = "<ol><li>Water melon</li><li>Grapes</li><li>Dates</li></ol>";
+//     document.getElementById("textContent").innerHTML = content;
+// }
